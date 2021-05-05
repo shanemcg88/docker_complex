4f938d6a3484c01169a7c6328e9a9f586c646078
Shane McGuire <shanelgmcguire@gmail.com>
2021-05-05 09:25:05 -0600

    replaced code with tutorial files and added dockerfiles

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # docker_complex
 
-Tutorial on building a Multi-Container Application with Docker
+Tutorial on building a Multi-Container Application with Docker.
+
+May have to run these commands in terminal for to build docker containers:
+
+export DOCKER_BUILDKIT=0
+export COMPOSE_DOCKER_CLI_BUILD=0
--- client/src/OtherPage.js
+++ client/src/OtherPage.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
 export default () => {
-    return (
-        <div>
-            I'm another page!
-            <Link to='/'>Go back to home</Link>
-        </div>
-    );
+  return (
+    <div>
+      Im some other page!
+      <Link to="/">Go back home</Link>
+    </div>
+  );
 };
